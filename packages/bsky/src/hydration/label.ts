ef70d3c7d0d3973aed9394e5f7ef28f1d84a99d4
futurGH <me@futuristick.ca>
2025-01-13 14:10:02 -0500

    don't hydrate labeler aggregates either

--- packages/bsky/src/hydration/label.ts
+++ packages/bsky/src/hydration/label.ts
@@ -172,14 +172,15 @@ export class LabelHydrator {
     const refs = dids.map((did) => ({ uri: labelerDidToUri(did) }))
     const key = Math.random().toString(16).slice(2)
     console.time(`getLabelerAggregates ${key}`)
-    const counts = await this.dataplane.getInteractionCounts({
+    // const counts = await this.dataplane.getInteractionCounts({
       refs,
       skipCacheForDids: viewer ? [viewer] : undefined,
     })
     console.timeEnd(`getLabelerAggregates ${key}`)
     return dids.reduce((acc, did, i) => {
       return acc.set(did, {
-        likes: counts.likes[i] ?? 0,
+        // likes: counts.likes[i] ?? 0,
+        likes: 0,
       })
     }, new HydrationMap<LabelerAgg>())
   }
