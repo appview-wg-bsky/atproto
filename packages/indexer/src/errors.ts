29b87290f10e665cdf6a9dc6885335c1c06827fc
futurGH <me@futuristick.ca>
2025-02-14 19:36:33 -0500

    Testing

--- packages/indexer/src/errors.ts
+++ packages/indexer/src/errors.ts
@@ -1,11 +1,11 @@
 export class FirehoseSubscriptionError extends Error {
   constructor(err: unknown) {
-    super('Error in firehose subscription', { cause: err })
+    super('error in firehose subscription', { cause: err })
   }
 }
 
 export class FirehoseWorkerError extends Error {
   constructor(err: unknown) {
-    super('Error in firehose worker', { cause: err })
+    super('error in firehose worker', { cause: err })
   }
 }
